63f57a60efd54756b226529c2b8b546aac6c659f
chicichich <pch6789@naver.com>
2021-09-15 16:47:53 +0900

    feat: list 클릭시 detail 페이지로 전환

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,20 @@
 import Component from "./core/Component.js";
 import List from "./view/List.js";
+import Detail from "./view/Detail.js";
 class App extends Component {
+  setup() {
+    this.state = { view: 'list' };
+  }
   async mounted() {
-    this.listView()
+    const { view } = this.state
+    switch (view) {
+      case 'list':
+        this.listView();
+        break;
+      case 'detail':
+        this.Detail();
+        break;
+    }
   }
   async listView() {
     const res = await fetch('http://127.0.0.1:3000/')
@@ -10,10 +22,18 @@ class App extends Component {
     const list = new List(this.$target, {
       itemList: [], clickEvent: ({ target }) => {
         const id = target.dataset.id
-        window.history.pushState({ data: id }, '품목', `/detail/${id}`)
+        history.pushState({ id: id }, '품목', `/detail/${id}`)
+        this.setState({ view: 'detail' })
       }
     });
     list.setState({ itemList })
+  }
+  async Detail() {
+    const { id } = history.state
+    const res = await fetch(`http://127.0.0.1:3000/options?id=${id}`)
+    const itemInfo = await res.json();
+    console.log(itemInfo)
+    const list = new Detail(this.$target, {});
   }
 }
 
