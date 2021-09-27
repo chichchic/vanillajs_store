1440669f113fa60503f7a5b2b5ff97c42fd3b0e0
chicichich <pch6789@naver.com>
2021-09-27 17:27:08 +0900

    feat: List component 추가

--- frontend/src/view/List.js
+++ frontend/src/view/List.js
@@ -1,26 +1,37 @@
 import Component from '../core/Component.js';
+import { request } from '../utility/api.js'
 
 class List extends Component {
+  data() {
+    return {
+      itemList: []
+    }
+  }
   template() {
-    const { itemList } = this.state;
+    const { itemList } = this.getData(['itemList']);
     return `
-    <ul class="item-list">
-    ${itemList.map(({ id, imgSrc, name }) => `
-      <li class="item-card" data-id="${id}">
-        <img
-          class="fit-picture"
-          src="${imgSrc}"
-          alt="${name}"
-        >
-        <p>${name}</p>
-      </li>
-    `).join('')}
-    </ul>
-    <button type="button" class="go-cart">카트로 이동하기</button>
+    <section class="list">
+      <ul class="item-list">
+      ${itemList.map(({ id, imgSrc, name }) => `
+        <li class="item-card" data-id="${id}">
+          <img
+            class="fit-picture"
+            src="${imgSrc}"
+            alt="${name}"
+          >
+          <p>${name}</p>
+        </li>
+      `).join('')}
+      </ul>
+      <button type="button" class="go-cart">카트로 이동하기</button>
+    </section>
     `
   }
+  async mounted() {
+    this.state.itemList = await request();
+  }
   setEvent() {
-    const { clickEvent, goCart } = this.state;
+    const { clickEvent, goCart } = this.methods;
     this.addEvent('click', '.item-card', clickEvent)
     this.addEvent('click', '.go-cart', goCart)
   }
