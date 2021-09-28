be2afa70c9377d6d1b69c1fc4b6078ee02c429a5
chicichich <pch6789@naver.com>
2021-09-28 10:38:31 +0900

    feat: Detail 컴포넌트 수정

--- frontend/src/view/Detail.js
+++ frontend/src/view/Detail.js
@@ -1,13 +1,28 @@
 import Component from "../core/Component.js";
 import SelectedList from "../components/SelectedList.js";
+import { request } from '../utility/api.js'
 import { spearator } from '../utility/separator.js'
 
 class Detail extends Component {
+  data() {
+    return {
+      cost: 0,
+      id: 0,
+      imgSrc: null,
+      name: '',
+      options: [],
+      selectedItemList: {}
+    }
+  }
   setup() {
-    this.state.selectedItemList = {};
+    this.setChild('.selected-list', () => SelectedList, ['selectedItemList', 'cost'], {
+      setSelectedItemList: (fn) => {
+        this.state.selectedItemList = fn(this.state.selectedItemList)
+      }
+    })
   }
   template() {
-    const { cost, id, imgSrc, name, options } = this.state;
+    const { cost, id, imgSrc, name, options } = this.getData(['cost', 'id', 'imgSrc', 'name', 'options']);
     return `
       <article class="detail">
         <section class="img-box">
@@ -41,30 +56,35 @@ class Detail extends Component {
       </article>
     `
   }
-  mounted() {
-    const { selectedItemList, cost } = this.state
-    const selectedList = new SelectedList(document.querySelector('.selected-list'), {
-      selectedItemList, defaultCost: cost, changeEvent: (e) => {
-        const id = e.target.dataset.id
-        e.target.value = Math.max(e.target.min, e.target.value)
-        e.target.value = Math.min(e.target.max, e.target.value)
-        this.setState({
-          selectedItemList: {
-            ...this.state.selectedItemList,
-            [id]: {
-              ...this.state.selectedItemList[id],
-              counter: e.target.value
-            }
-          }
-        })
-      }
-    })
-    this.appendChild('selectedList', selectedList);
+  async created() {
+    const itemId = history.state.id;
+    const { id, name, imgSrc, cost, options } = await request(itemId);
+    this.state.id = id;
+    this.state.name = name;
+    this.state.imgSrc = imgSrc;
+    this.state.cost = cost;
+    this.state.options = options;
   }
   setEvent() {
-    const { changeEvent, clickEvent } = this.state;
-    this.addEvent('change', '.option-selet', changeEvent)
-    this.addEvent('click', '.put-button', clickEvent)
+    const { setView, setCart } = this.emits;
+    this.addEvent('change', '.option-selet', (e) => {
+      const optionIndex = e.target.selectedIndex;
+      if (optionIndex <= 0) return;
+      const selectedOption = this.state.options[optionIndex - 1]
+      if (this.state.selectedItemList[selectedOption.id] !== undefined) return;
+      const newState = { [selectedOption.id]: { ...selectedOption, counter: 1 } }
+      this.state.selectedItemList = { ...newState, ...this.state.selectedItemList }
+    })
+    this.addEvent('click', '.put-button', () => {
+      const { id } = history.state;
+      history.pushState(null, '품목', `/`)
+      if (Object.keys(this.state.selectedItemList).length === 0) {
+        setView(() => 'list')
+        return;
+      }
+      setCart((oldVal) => ({ ...oldVal, [id]: this.state.selectedItemList }))
+      setView(() => 'list')
+    })
   }
 }
 
