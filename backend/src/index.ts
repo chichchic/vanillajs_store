b50d3bf6c85417db50a50fd28cd0e43834c979e0
chicichich <pch6789@naver.com>
2021-09-30 00:45:49 +0900

    refactor: backend 경로 변경

---
+++ backend/src/index.ts
@@ -0,0 +1,25 @@
+import express from "express";
+import cors from "cors";
+import path from "path";
+
+import { list, options } from "src/data/list";
+
+const __dirname = path.resolve();
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+app.use("/static", express.static(__dirname + "/public"));
+
+app.get("/", (req, res) => {
+  res.json(list);
+});
+
+app.get("/options", (req: express.Request, res) => {
+  const itemId = Number(req.query.id);
+  const listIndex = list.findIndex(({ id }) => id === itemId);
+  const result = { ...list[listIndex], options: options[itemId - 1] };
+  res.json(result);
+});
+
+app.listen(3000, () => console.log("server start"));
