76fa15b3d55e32a0f0557737aa0d21505901a2b2
chicichich <pch6789@naver.com>
2021-10-05 00:01:25 +0900

    feat: set /api/product/ route

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,5 +1,8 @@
 import express from "express";
+import { getList, getOptionsById } from "src/controller/products";
 
 const productsRoutes = express.Router();
+productsRoutes.get("/getList", getList);
+productsRoutes.get("/getOptionsById/:productId", getOptionsById);
 
 export { productsRoutes };
