e0788efd048b7ebdc5536b4e7c31961942da230e
chicichich <pch6789@naver.com>
2021-10-04 21:14:15 +0900

    feat: backend 폴더 구조 변경

---
+++ backend/src/main.ts
@@ -0,0 +1,18 @@
+import express from "express";
+import cors from "cors";
+import path from "path";
+
+import { productsRoutes } from "src/routes";
+
+const PORT = 3000;
+
+const __dirname = path.resolve();
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+app.use("/static", express.static(__dirname + "/public"));
+
+app.use("/api/product", productsRoutes);
+
+app.listen(PORT, () => console.log(`Running on port ${PORT}`));
