a740e241586f1e4d9f2f0b6d81ac8b4b46d08d26
chicichich <pch6789@naver.com>
2021-10-04 23:39:01 +0900

    feat: Implement product get

---
+++ backend/src/controller/products.ts
@@ -0,0 +1,29 @@
+import express from "express";
+
+import { extractOptions } from "src/utility/extract";
+
+import list from "src/data/list.json";
+import options from "src/data/options.json";
+
+function getList(req: express.Request, res: express.Response) {
+  res.status(200).json(list);
+}
+
+function getOptionsById(
+  req: express.Request,
+  res: express.Response,
+  next: Function
+) {
+  try {
+    const result = extractOptions(Number(req.params.productId));
+    if (result === null) {
+      res.status(404).end();
+    } else {
+      res.status(200).json(result);
+    }
+  } catch (error) {
+    next(error);
+  }
+}
+
+export { getList, getOptionsById };
