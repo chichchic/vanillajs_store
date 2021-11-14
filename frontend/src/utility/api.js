277e24eb8283c088d5713dba3582e011b54f0cc4
chicichich <pch6789@naver.com>
2021-11-14 19:12:42 +0900

    feat: modify api path

--- frontend/src/utility/api.js
+++ frontend/src/utility/api.js
@@ -1,7 +1,7 @@
-const API_END_POINT = 'http://127.0.0.1:3000'
+const API_END_POINT = 'http://127.0.0.1:3000/api/product'
 export async function request(id) {
   try {
-    const res = await fetch(`${API_END_POINT}/${id ? `options?id=${id}` : ''}`)
+    const res = await fetch(`${API_END_POINT}/${id ? `getOptionsById/${id}` : 'getList'}`)
     if (!res.ok) {
       throw new Error('server error');
     }
